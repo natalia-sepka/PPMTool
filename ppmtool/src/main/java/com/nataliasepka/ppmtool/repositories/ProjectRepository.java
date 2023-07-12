78163dccf3c783b0876fc9478fcde067585d5b61
natalia.sepka <natalia.sepka@gmail.com>
2023-07-12 19:48:18 +0200

    Create and review Boiler Plate react app

--- ppmtool/src/main/java/com/nataliasepka/ppmtool/repositories/ProjectRepository.java
+++ ppmtool/src/main/java/com/nataliasepka/ppmtool/repositories/ProjectRepository.java
@@ -1,6 +1,8 @@
 package com.nataliasepka.ppmtool.repositories;
 
 import com.nataliasepka.ppmtool.domain.Project;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,4 @@ public interface ProjectRepository extends CrudRepository<Project, Long> {
     Project findByProjectIdentifier(String projectId);
     @Override
     Iterable<Project> findAll();
-
 }
