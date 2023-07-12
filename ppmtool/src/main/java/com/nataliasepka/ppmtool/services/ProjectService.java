54077efc3c74a7f36f0441575f38c7bd95ea839c
natalia.sepka <natalia.sepka@gmail.com>
2023-07-12 10:50:24 +0200

    Custom exceptions for unique Project identifiers

--- ppmtool/src/main/java/com/nataliasepka/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/nataliasepka/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.nataliasepka.ppmtool.services;
 
 import com.nataliasepka.ppmtool.domain.Project;
+import com.nataliasepka.ppmtool.exceptions.ProjectIdException;
 import com.nataliasepka.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,13 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+ "'already exists");
+        }
+
+
     }
 }
