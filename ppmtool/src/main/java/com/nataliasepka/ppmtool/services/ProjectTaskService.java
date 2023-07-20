7ed0e043499d689f9ffe596908529f8a377a3153
natalia.sepka <natalia.sepka@gmail.com>
2023-07-20 12:00:08 +0200

    Delete operation bug fix, improved backlog/project task relationship

--- ppmtool/src/main/java/com/nataliasepka/ppmtool/services/ProjectTaskService.java
+++ ppmtool/src/main/java/com/nataliasepka/ppmtool/services/ProjectTaskService.java
@@ -90,11 +90,7 @@ public class ProjectTaskService {
     }
 
     public void deletePTByProjectSequence(String backlog_id, String pt_id) {
-        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
-        Backlog backlog = projectTask.getBacklog();
-        List<ProjectTask> pts = backlog.getProjectTasks();
-        pts.remove(projectTask);
-        backlogRepository.save(backlog);
+          ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
         projectTaskRepository.delete(projectTask);
     }
 
